f830dcfa616d5f54bbb3bb6e7b9b3a16bc5534ad
kanat1390 <atygayev.kanat@gmail.com>
2022-05-19 23:07:32 +0600

    added design and templates

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,13 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        queryset = super(PublishedManager, self).get_queryset().filter(
+            status='published')
+        return queryset
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -11,14 +18,16 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='blog_posts')
-    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
         max_length=10, choices=STATUS_CHOICES, default='draft')
+    image = models.ImageField(upload_to="images")
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish',)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 # Create your views here.
+
+
+def post_list(request):
+    posts = Post.published.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/post/list.html', context)
+
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug, status='published')
+    context = {
+        'post': post,
+    }
+    return render(request, 'blog/post/detail.html', context)
