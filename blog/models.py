0b81844c9b0d4de18e6586cda49ed35eccd26104
kanat1390 <atygayev.kanat@gmail.com>
2022-05-20 14:51:07 +0600

    add pagination

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
 
 
@@ -34,3 +35,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", kwargs={"slug": self.slug, "year": self.publish.year, "month": self.publish.month, "day": self.publish.day})
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 
 def post_list(request):
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, per_page=4)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     context = {
         'posts': posts,
     }
     return render(request, 'blog/post/list.html', context)
 
 
-def post_detail(request, slug):
-    post = get_object_or_404(Post, slug=slug, status='published')
+def post_detail(request, slug, year, month, day):
+    post = get_object_or_404(Post, slug=slug, status='published',
+                             publish__year=year, publish__month=month, publish__day=day)
     context = {
         'post': post,
     }
